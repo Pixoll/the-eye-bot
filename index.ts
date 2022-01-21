2bd86ccbb8ec44c731998dd8e5658de331ad8b09
Pixoll <pixoll.gamer@gmail.com>
2022-01-21 17:52:53 -0300

    JS -> TS

--- index.ts
+++ index.ts
@@ -1,28 +1,27 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const discord_js_1 = require("discord.js");
-const dotenv_1 = __importDefault(require("dotenv"));
-dotenv_1.default.config();
-const client = new discord_js_1.Client({
+import { Client, User } from 'discord.js';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const client = new Client({
     intents: ['GUILD_PRESENCES']
 });
+
 client.on('ready', () => console.log('The client is now online.'));
+
 const pixelId = '802267523058761759';
 const pixollId = '667937325002784768';
-let pixoll;
+let pixoll: User;
+
 client.on('presenceUpdate', async (old, { userId, status }) => {
-    if (userId !== pixelId || old?.status === status)
-        return;
+    if (userId !== pixelId || old?.status === status) return;
+
     try {
         pixoll ??= await client.users.fetch(pixollId);
         await pixoll.send(`<@802267523058761759> just went \`${status}\`.`);
-    }
-    catch (err) {
+    } catch (err) {
         console.error(err);
         console.log(`Pixel just went "${status}".`);
     }
 });
+
 client.login();
